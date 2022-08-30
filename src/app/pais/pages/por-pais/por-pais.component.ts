8ce65fed0a625b9f0cec4209973c5ebc98fbb2bf
Erika <erikapalmasan@gmail.com>
2022-08-30 17:56:06 +0200

    Servicio para los países - Manejo de errores en la petición get

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,29 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.scss']
+  styleUrls: ['./por-pais.component.scss'],
 })
 export class PorPaisComponent implements OnInit {
+  termino: string = '';
+  isError: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscar(): void {
+    this.isError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        this.isError = true;
+      }
+    );
   }
-
 }
